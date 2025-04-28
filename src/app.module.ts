84d3c9d041dbd71b5364d3d34b2a5e008ea389b3
tini <martynas.raiselis@gmail.com>
2025-04-28 13:01:02 +0200

    feat: remove unsused files, add test helloWorld module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HelloWorldModule],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
