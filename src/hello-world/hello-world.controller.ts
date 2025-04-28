33485513433d935483be5355ea6bdab4e5c2ffa3
tini <martynas.raiselis@gmail.com>
2025-04-28 13:23:22 +0200

    feat(swagger): add swagger and update hello world controller to use swagger

--- src/hello-world/hello-world.controller.ts
+++ src/hello-world/hello-world.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { HelloWorldService } from './hello-world.service';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 
 @Controller('hello-world')
 export class HelloWorldController {
-    constructor(private readonly hello: HelloWorldService) {}
+    constructor(private readonly hello: HelloWorldService) { }
 
-    @Get('test')
-    getHello(): string {
-        return this.hello.getHello();
+    @Get(':name')
+    @ApiParam({
+        name: 'name',
+        type: String,
+        description: 'Your name',
+        example: 'John Doe',
+    })
+    @ApiOperation({ summary: 'gets you a hellow world with your name' })
+    @ApiResponse({
+        status: 200,
+        description: 'all gucci',
+    })
+    getHello(
+        @Param('name') name: string,
+    ): string {
+        return this.hello.getHello() + name;
     }
 }
