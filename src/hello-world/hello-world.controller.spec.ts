1190685269ff45859a6deea5b5622ca94bfa5075
David Ajken <david.ajken@hotmail.com>
2025-04-30 10:23:10 +0200

    my first test

--- src/hello-world/hello-world.controller.spec.ts
+++ src/hello-world/hello-world.controller.spec.ts
@@ -1,18 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HelloWorldController } from './hello-world.controller';
+import { HelloWorldService } from './hello-world.service';
 
 describe('HelloWorldController', () => {
     let controller: HelloWorldController;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
+            providers: [HelloWorldService],
             controllers: [HelloWorldController],
         }).compile();
 
-        controller = module.get<HelloWorldController>(HelloWorldController);
+        controller = await module.get<HelloWorldController>(HelloWorldController);
     });
 
     it('should be defined', () => {
         expect(controller).toBeDefined();
     });
+    it("test", () => {
+        const actual = controller.getHello("test");
+        expect(actual).toBe("Hello World!test")
+    })
 });
