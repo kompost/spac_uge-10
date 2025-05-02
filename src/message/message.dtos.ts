7b99167c0d8c6ee2954655b93c39f050b33c7418
David Ajken <david.ajken@hotmail.com>
2025-05-02 11:21:23 +0200

    added dto validatio

--- src/message/message.dtos.ts
+++ src/message/message.dtos.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {IsString} from 'class-validator'
 
 export class CreateMessageDTO {
     @ApiProperty()
+    @IsString()
     userId: string;
     @ApiProperty()
+    @IsString()
     chatroomId: string;
     @ApiProperty()
+    @IsString()
     message: string;
 }
