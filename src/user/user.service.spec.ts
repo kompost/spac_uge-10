a32ec97db23347d58e2f47dc5d4aa1f9d310c574
David Ajken <david.ajken@hotmail.com>
2025-05-02 09:29:52 +0200

    get ready on user

---
+++ src/user/user.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from './user.service';
+
+// describe('HelloWorldService', () => {
+//     let service: HelloWorldService;
+
+//     beforeEach(async () => {
+//         const module: TestingModule = await Test.createTestingModule({
+//             providers: [HelloWorldService],
+//         }).compile();
+
+//         service = module.get<HelloWorldService>(HelloWorldService);
+//     });
+
+//     it('should be defined', () => {
+//         expect(service).toBeDefined();
+//     });
+// });
