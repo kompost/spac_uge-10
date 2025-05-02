376b17c1770709bb7195309231a163de25891a20
tDog - Tini <martynas.raiselis@gmail.com>
2025-05-02 09:44:39 +0200

    feat(sockets): add socket connection for chat function

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { HelloWorldModule } from './hello-world/hello-world.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ChatGateway } from './events/events.gateway';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-    imports: [HelloWorldModule, PrismaModule],
+    imports: [
+        HelloWorldModule,
+        PrismaModule,
+        ServeStaticModule.forRoot({
+            rootPath: join(process.cwd(), 'public'),
+        }),
+    ],
     controllers: [],
-    providers: [],
+    providers: [ChatGateway],
 })
-export class AppModule {}
+export class AppModule { }
