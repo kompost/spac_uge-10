0bc1a3451450e6eb2168c876bb67466fa4684b24
kompost <martynas.raiselis@gmail.com>
2025-05-02 09:52:26 +0200

    feat(sockets): add socket connection for chat function (#18)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { HelloWorldModule } from './hello-world/hello-world.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ChatGateway } from './events/events.gateway';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { UserModule } from './user/user.module';
+import { join } from 'path';
 
 @Module({
-    imports: [HelloWorldModule, PrismaModule, UserModule],
+    imports: [
+        HelloWorldModule,
+        PrismaModule,
+        UserModule,
+        ServeStaticModule.forRoot({
+            rootPath: join(process.cwd(), 'public'),
+        }),
+    ],
     controllers: [],
-    providers: [],
+    providers: [ChatGateway],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/events/events.gateway.ts
@@ -0,0 +1,46 @@
+import {
+    WebSocketGateway,
+    SubscribeMessage,
+    MessageBody,
+    ConnectedSocket,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+
+@WebSocketGateway({ cors: true })
+export class ChatGateway {
+    constructor(
+        private readonly server: Server, // Inject the server instance
+    ) { }
+
+    handleConnection(client: Socket) {
+        console.log(`Client connected: ${client.id}`);
+    }
+
+    handleDisconnect(client: Socket) {
+        console.log(`Client disconnected: ${client.id}`);
+    }
+
+    @SubscribeMessage('joinRoom')
+    handleJoinRoom(@MessageBody() room: string, @ConnectedSocket() client: Socket) {
+        client.join(room);
+        client.emit('joinedRoom', room);
+    }
+
+    @SubscribeMessage('leaveRoom')
+    handleLeaveRoom(@MessageBody() room: string, @ConnectedSocket() client: Socket) {
+        client.leave(room);
+        client.emit('leftRoom', room);
+    }
+
+    @SubscribeMessage('chatToServer')
+    handleMessage(
+        @MessageBody() payload: { room: string; message: string },
+        @ConnectedSocket() client: Socket,
+    ) {
+        client.to(payload.room).emit('chatToClient', {
+            sender: client.id,
+            message: payload.message,
+        });
+    }
+}
+
