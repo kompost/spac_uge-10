795658e78b404aeabbb392679f33ca733db12ea7
David Ajken <david.ajken@hotmail.com>
2025-05-07 13:42:56 +0200

    pathing

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { User } from '@prisma/client';
 import { hash } from 'bcrypt';
 
